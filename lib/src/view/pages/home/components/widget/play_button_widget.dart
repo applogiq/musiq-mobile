24b4a0a32683b83afbd8ddf42b8f0cbaaa463c38
Shajith <shajithali.s.applogiq@gmail.com>
2022-06-06 18:21:00 +0530

    Player Screen

--- lib/src/view/pages/home/components/widget/play_button_widget.dart
+++ lib/src/view/pages/home/components/widget/play_button_widget.dart
@@ -3,21 +3,27 @@ import 'package:flutter/material.dart';
 import '../../../../../helpers/constants/color.dart';
 
 class PlayButtonWidget extends StatelessWidget {
-  const PlayButtonWidget({
+   PlayButtonWidget({
     Key? key,
+    this.bgColor=const Color.fromRGBO(255, 255, 255, 0.8),
+    this.iconColor=const Color.fromRGBO(254, 86, 49, 1),
+    this.size=20.0,this.padding=6.0
   }) : super(key: key);
-
+var bgColor;
+var iconColor;
+var size;
+var padding;
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(8),
-      padding: EdgeInsets.all(6),
+      padding: EdgeInsets.all(padding),
       decoration:
-          BoxDecoration(color: CustomColor.playIconBg, shape: BoxShape.circle),
+          BoxDecoration(color: bgColor, shape: BoxShape.circle),
       child: Icon(
         Icons.play_arrow_rounded,
-        size: 20,
-        color: CustomColor.secondaryColor,
+        size: size,
+        color: iconColor,
       ),
     );
   }
